0c85dbc5c68b6dcb8204295e47b88a00d520be5d
Danylo Malyuta <danylo.malyuta@gmail.com>
2017-12-17 13:53:45 +0100

    License updates

--- apriltags2_ros/include/apriltags2_ros/common_functions.h
+++ apriltags2_ros/include/apriltags2_ros/common_functions.h
@@ -1,3 +1,46 @@
+/**
+ * Copyright (c) 2017, California Institute of Technology.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of the California Institute of
+ * Technology.
+ *
+ ** common_functions.h *********************************************************
+ *
+ * Wrapper classes for AprilTag standalone and bundle detection. Main function
+ * is TagDetector::detectTags which wraps the call to core AprilTags 2
+ * algorithm, apriltag_detector_detect().
+ *
+ * $Revision: 1.0 $
+ * $Date: 2017/12/17 13:23:14 $
+ * $Author: dmalyuta $
+ *
+ * Originator:        Danylo Malyuta, JPL
+ ******************************************************************************/
+
 #ifndef APRILTAGS2_ROS_COMMON_FUNCTIONS_H
 #define APRILTAGS2_ROS_COMMON_FUNCTIONS_H
 
--- apriltags2_ros/scripts/calibrate_bundle.m
+++ apriltags2_ros/scripts/calibrate_bundle.m
@@ -1,3 +1,34 @@
+% Copyright (c) 2017, California Institute of Technology.
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are met:
+%
+% 1. Redistributions of source code must retain the above copyright notice,
+%    this list of conditions and the following disclaimer.
+% 2. Redistributions in binary form must reproduce the above copyright notice,
+%    this list of conditions and the following disclaimer in the documentation
+%    and/or other materials provided with the distribution.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+%
+% The views and conclusions contained in the software and documentation are
+% those of the authors and should not be interpreted as representing official
+% policies, either expressed or implied, of the California Institute of
+% Technology.
+%
+%%% calibrate_bundle.m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 % Script to determine AprilTag bundle relative poses to a "master" tag.
 %
 % Instructions:
@@ -6,6 +37,13 @@
 %   individual tags are visible at least once at some point (the more the
 %   better). Run the script, then copy the printed output into the tag.yaml
 %   configuration file of apriltags2_ros.
+%
+% $Revision: 1.0 $
+% $Date: 2017/12/17 13:37:34 $
+% $Author: dmalyuta $
+%
+% Originator:        Danylo Malyuta, JPL
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% User inputs
 
