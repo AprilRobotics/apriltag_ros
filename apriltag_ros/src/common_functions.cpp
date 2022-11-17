2c77542854871d3c9874d2d7e997717a263abae6
francesco <francesco.fallica@tugbot.com>
2022-11-17 12:33:35 +0000

    fixed bug into function TagDetector::idComparison. previously it was assigning to id1 and id2 the addresses of the pointers. now, it compares the tag id.

--- apriltag_ros/src/common_functions.cpp
+++ apriltag_ros/src/common_functions.cpp
@@ -404,8 +404,8 @@ AprilTagDetectionArray TagDetector::detectTags (
 
 int TagDetector::idComparison (const void* first, const void* second)
 {
-  int id1 = ((apriltag_detection_t*) first)->id;
-  int id2 = ((apriltag_detection_t*) second)->id;
+  int id1 = (*(apriltag_detection_t**)first)->id;
+  int id2 = (*(apriltag_detection_t**)first)->id;
   return (id1 < id2) ? -1 : ((id1 == id2) ? 0 : 1);
 }
 
